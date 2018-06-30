4d368e68b5da229b75c44e2e96060eeb5aef69a2
VraizeX <vraizen@mail.ru>
2018-06-30 03:05:00 +0300

    C# part 2

---
+++ Troelsen2/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen2
+{
+    class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        public string GetName()
+        {
+            return empName;
+        }
+        public void SetName(string name)
+        {
+            if (name.Length > 15)
+                Console.WriteLine("Error");
+            else
+                empName = name;
+
+        }
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplaySt()
+        {
+            Console.WriteLine("Name {0}", empName);
+            Console.WriteLine("ID: {0}",empID);
+            Console.WriteLine("Pay: {0}",currPay);
+        }
+    }
+
+}
---
+++ Troelsen2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Marvin",456,3000);
+            emp.GiveBonus(1000);
+            emp.DisplaySt();
+            emp.SetName("Marvxdfbzdfbzdfbgfgzdfg");
+            Console.WriteLine("name {0}", emp.GetName());
+            Console.ReadLine();
+
+            
+        }
+    }
+}
