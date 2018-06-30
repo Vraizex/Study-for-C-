c3fecc6d0652c5e418c81f46717be996d3c73d50
VraizeX <vraizen@mail.ru>
2018-06-30 03:05:40 +0300

    Part 3

---
+++ Troelsen3/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen3
+{
+        class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error");
+                else
+                    empName = value;
+            }
+        }
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+
+        }
+        //public string SocSectNumber
+        //{
+        //    get { return empSSN; }
+        //}
+        public Employee() { }
+        public Employee(string name, int id, float pay) : this(name, 0, id, pay) { }
+
+        public Employee(string name,int age, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            empAge = age;
+            currPay = pay;
+        }
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplaySt()
+        {
+            Console.WriteLine("Name {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Age: {0}", empAge);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+
+}
---
+++ Troelsen3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MOVE SOFTWARE");
+            Employee emp = new Employee("NAMREEE", 460,300000);
+            Employee joy = new Employee();
+            joy.Age++;
+            emp.GiveBonus(1000);
+            emp.DisplaySt();
+            emp.Name = "Pepe";
+            Console.WriteLine("sdgasdg {0}",emp.Name);
+            
+            Console.ReadLine();
+        }
+    }
+}
