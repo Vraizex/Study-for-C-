b78bc70faa233889bdb28de0b81a68fd4b17439e
VraizeX <vraizen@mail.ru>
2018-06-30 03:06:45 +0300

    Part 4

---
+++ Troelsen215/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen215
+{
+    class Car
+    {
+        public string PetMame { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+        public void Disp()
+        {
+            Console.WriteLine("Car Name:{0}",PetMame);
+            Console.WriteLine("Speed:{0}",Speed);
+            Console.WriteLine("Color:{0}",Color);
+        }
+    }
+}
---
+++ Troelsen215/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troelsen215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Staaarted program");
+            Car c = new Car();
+            c.PetMame = "Fread";
+            c.Speed = 55;
+            c.Color = "Red";
+            c.Disp();
+            Garage g = new Garage();
+            g.MyAuto = c;
+            Console.WriteLine("Number car:{0}", g.NumberOfCars);
+            Console.WriteLine("Имя:{0}",g.MyAuto.PetMame);
+       
+            Console.ReadLine();
+        }
+    }
+}
